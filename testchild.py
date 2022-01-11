7a2a98dd944f248e83d7427c1808c109df7b7f66
David Dudley <dmddudley@gmail.com>
2022-01-10 16:30:12 -0800

    added a new element to testchild.py

--- testchild.py
+++ testchild.py
@@ -1,3 +1,15 @@
 ## Adding a new file in child branch
 print ("Inside Child Branch")
 print ("Inside Child Branch")
+
+student_grades = [9.1, 8.8, 7.5]
+student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
+
+
+mysum = sum(student_grades)
+length = len(student_grades)
+
+mean = mysum/length
+
+print(mean)
+
